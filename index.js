0f42f1f61c8861e88c919fb8df06f6e79aee959a
Shashank Shekhar <shashank7200@gmail.com>
2018-07-31 17:38:03 +0530

    added logic to examine the connectivity status

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 /*              IMPORTS                      */
 
-const { app, BrowserWindow } = require("electron");
-const Offline = require('offline-js');
-
+const { app, BrowserWindow, ipcMain } = require("electron");
 
 let win;
 
@@ -12,37 +10,39 @@ const createWindow = () => {
   // creates the browser window
   win = new BrowserWindow({ width: "60%", height: "60%" });
 
-  // loads the initial file
-  win.loadURL('https://electronjs.org/');
+  ipcMain.on("online-status-changed", (event, status) => {
+    console.log(status);
+  });
 
-  win.on('closed', () => {
+  // loads the initial file
+  win.loadURL("https://electronjs.org/");
+
+  win.on("closed", () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
 
     win = null;
-  })
+  });
 };
 
 // creates the interface when the app is ready to be rendered
-app.on('ready', createWindow);
-
+app.on("ready", createWindow);
 
 // Listener for the event, when the user closes all the windows
-app.on('window-all-closed', () => {
+app.on("window-all-closed", () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
+  if (process.platform !== "darwin") {
+    app.quit();
   }
 });
 
-
 // Listener for the app, when the window resumes
-app.on('activate', () => {
+app.on("activate", () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (win === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
